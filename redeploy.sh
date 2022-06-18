1f549dc7ddd248913fcf20e065fd7cf7585b3e38
smrithi-c <smrithic@terpmail.umd.edu>
2022-06-18 22:11:08 +0000

    updating redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,2 +1,2 @@
 pkill tmux
-tmux new-session -d -s server 'cd project-404-team-name-not-found && git fetch && git reset origin/main --hard && python -m venv python3-virtualenv && source python3-virtualenv/bin/activate && pip install -r requirements.txt && flask run --host=0.0.0.0 --port=80'
+tmux new-session -d -s server 'git fetch && git reset origin/main --hard && python -m venv python3-virtualenv && source python3-virtualenv/bin/activate && pip install -r requirements.txt && flask run --host=0.0.0.0 --port=80'
