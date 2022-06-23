a4b179a660cc2e1b214297542fa54cc80ad4a740
smrithi-c <smrithic@terpmail.umd.edu>
2022-06-23 14:12:39 -0400

    added curl-test file

---
+++ curl-test.sh
@@ -0,0 +1,16 @@
+
+NAME=$(openssl rand -hex 6)
+echo $NAME
+curl --request POST http://localhost:5000/api/timeline_post -d 'name='"$NAME"'&email=test@test.com&content=Test Content!'
+LNAME=$(curl http://localhost:5000/api/timeline_post|jq .'timelineposts[0].name')
+echo $LNAME
+if [ "$LNAME"=="$NAME" ];
+then 
+	echo successfully added!
+else 
+	echo LNAME
+	echo NAME
+fi
+
+
+curl -X "DELETE" 'http://localhost:5000/api/timeline_post'
