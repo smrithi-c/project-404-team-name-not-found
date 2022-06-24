f3eac0f08a2bb876777474b7f5e53b895f9f2b43
smrithi-c <smrithic@terpmail.umd.edu>
2022-06-24 18:04:52 +0000

    adding service based redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,2 +1,5 @@
-pkill tmux
-tmux new-session -d -s server 'git fetch && git reset origin/main --hard && python -m venv python3-virtualenv && source python3-virtualenv/bin/activate && pip install -r requirements.txt && flask run --host=0.0.0.0 --port=80'
+git fetch && git reset origin/main --hard
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+systemctl restart myportfolio
